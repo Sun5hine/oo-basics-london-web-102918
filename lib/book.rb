987a63aba383b09553fdf1ee770a2cd5375ea94f
sun5hine <af5kh@yahoo.com>
2018-11-01 18:04:29 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
-
+class Book
+  
+  def initialize(title)
+    @title = title 
+  end
+  
+  def title
+    @title
+  end 
+  
+  def autor =(author)
+    @author = author 
+  end 
+  
+  def author
+    @author 
+  end
+  
+  
+  
+end 
 
