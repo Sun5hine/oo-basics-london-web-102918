118d8fcff3a2fe8d68911314032a5c70cad652f9
sun5hine <af5kh@yahoo.com>
2018-11-01 18:05:59 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,7 +16,24 @@ class Book
     @author 
   end
   
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
   
+  def genre 
+    @genre 
+  end 
   
-end 
+  def turn_page
+    
+  end 
+ 
+end
+
+  
+
 
